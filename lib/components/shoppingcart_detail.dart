5922c6ec41d3cc518a705d8551669ca1c1640647
Lee Taeyoung <seen@korea.ac.kr>
2023-01-06 19:14:34 +0900

    shopping cart detail skeleton

--- lib/components/shoppingcart_detail.dart
+++ lib/components/shoppingcart_detail.dart
@@ -5,8 +5,40 @@ class ShoppingCartDetail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(40)
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _buildDetailNameAndPrice(),
+            _buildDetailRatingReviewCount(),
+            _buildDetailColorOptions(),
+            _buildDetailButton(context),
+          ],
 
+        ),
+      ),
     );
+  }
+
+  _buildDetailNameAndPrice() {
+    return SizedBox();
+  }
+
+  _buildDetailRatingReviewCount() {
+    return SizedBox();
+  }
+
+  _buildDetailColorOptions() {
+    return SizedBox();
+  }
+
+  _buildDetailButton(BuildContext context) {
+    return SizedBox();
   }
 }
