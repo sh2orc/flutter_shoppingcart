54ff76b68f5df8d0ed7f59c3b8584be0544c0826
Lee Taeyoung <seen@korea.ac.kr>
2023-01-06 19:04:19 +0900

    skeleton coding appbar

---
+++ lib/components/shoppingcart_detail.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ShoppingCartDetail extends StatelessWidget {
+  const ShoppingCartDetail({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+
+    );
+  }
+}
---
+++ lib/components/shoppingcart_header.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class ShoppingCartHeader extends StatefulWidget {
+  @override
+  State<ShoppingCartHeader> createState() => _ShoppingCartHeaderState();
+}
+
+class _ShoppingCartHeaderState extends State<ShoppingCartHeader> {
+  int selectedId = 0;
+
+  List<String> selectedPic = [
+    "assets/p1.jpeg",
+    "assets/p2.jpeg",
+    "assets/p3.jpeg",
+    "assets/p4.jpeg",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+    );
+  }
+}
